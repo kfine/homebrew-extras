d485603d0d838b23628e2cd942049227acc15ff4
kfine <kfine@lpl.arizona.edu>
2013-12-04 17:37:42 -0700

    adding torque-client

---
+++ torque-client.rb
@@ -0,0 +1,35 @@
+require 'formula'
+
+class Torque < Formula
+  homepage 'http://www.adaptivecomputing.com/products/open-source/torque/'
+  url 'http://www.adaptivecomputing.com/index.php?wpfb_dl=1643'
+  version '2.5.13'
+  sha1 '2290a8d9330bc9d7bcc37ec2e75b0d5d59e67e9c'
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--disable-mom",
+                          "--disable-unixsockets",
+                          "--disable-gui",
+                          "--with-default-server=torqueserver.lpl.arizona.edu",
+                          "--disable-gcc-warnings",
+                          "--with-tcp-retry-limit=2",
+                          "--disable-server"
+
+    system "make"
+    system "make", "install_clients"
+  end
+
+  def patches
+    [
+      "https://raw.github.com/kfine/homebrew-extras/master/patches/2.5.13-u_threadpool.c.patch"
+    ]
+  end
+
+  test do
+    system "#{bin}/qstat", "--version"
+  end
+end
