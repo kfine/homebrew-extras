ebbf1c50cb81d7739bc1b7cdb7ecf93ce65a0a34
kfine <kfine@lpl.arizona.edu>
2013-12-10 17:20:02 -0700

    Adding Casks

---
+++ Casks/hiview.rb
@@ -0,0 +1,7 @@
+class Hiview < Cask
+  url 'http://pirlwww.lpl.arizona.edu/software/HiView/HiView-Darwin-universal.dmg'
+  homepage 'http://hirise.lpl.arizona.edu/hiview/'
+  version '1.4.0'
+  sha1 '89fdda3d303179c98e2355c26c12a6bf882f60d7'
+  link 'HiView.app'
+end
---
+++ Casks/psi.rb
@@ -0,0 +1,7 @@
+class Psi < Cask
+  url 'http://downloads.sourceforge.net/project/psi/Psi/0.15/Psi-0.15.dmg'
+  homepage 'http://psi-im.org/'
+  version '0.15'
+  sha1 '9355c996a43fe4708d669ac06381945f5241ed83'
+  link 'Psi.app'
+end
