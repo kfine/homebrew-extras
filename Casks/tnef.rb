afe40faadac5100adac00535408e5a277c97d2b9
kfine <kfine@lpl.arizona.edu>
2014-02-04 16:44:25 -0700

    TNEF's Enough App for OSX

---
+++ Casks/tnef.rb
@@ -0,0 +1,7 @@
+class Hiview < Cask
+  url 'http://www.joshjacob.com/mac-development/TNEF3.2.1.dmg' 
+  homepage 'http://www.joshjacob.com/mac-development/tnef.php' 
+  version '3.2.1'
+  sha1 'c561bb94d19388ef335015767c0cc533bea63caf' 
+  link "TNEF's Enough.app"
+end
