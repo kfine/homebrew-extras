36a13323789b27d13d2d837084ec57f85a98d0a2
kfine <kfine@lpl.arizona.edu>
2014-10-30 14:39:22 -0700

    updating download

--- Casks/hiview.rb
+++ Casks/hiview.rb
@@ -2,6 +2,6 @@ class Hiview < Cask
   url 'http://pirlwww.lpl.arizona.edu/software/HiView/HiView-Darwin-universal.dmg'
   homepage 'http://hirise.lpl.arizona.edu/hiview/'
   version '1.4.0'
-  sha1 '89fdda3d303179c98e2355c26c12a6bf882f60d7'
+  sha256 '90f7a0f6e7269188a44eea3761180950ee7ba7a30971df1f53735b94e5cd61f5'
   link 'HiView.app'
 end
